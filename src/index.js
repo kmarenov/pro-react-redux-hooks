c4747b041adb6fe788b071e92c2ff581004f8991
Kirill Marenov <kmarenov@gmail.com>
2020-02-20 13:13:17 +0300

    96. useState()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <p>Hi</p>;
+const App = () => {
+    return (
+        <div>
+            <HookSwitcher />
+        </div>
+    );
+};
+
+const HookSwitcher = () => {
+    const [ color, setColor ] = useState('gray');
+    const [ fontSize, setFontSize ] = useState(14);
+
+    return (
+        <div style={{ padding: '10px', backgroundColor: color, fontSize: `${fontSize}px`}}>
+            Hello World
+            <button onClick={() => setColor('gray')}>Dark</button>
+            <button onClick={() => setColor('white')}>Light</button>
+            <button onClick={() => setFontSize((s) => s + 2)}>+</button>
+            <button onClick={() => setFontSize((s) => s - 2)}>-</button>
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
