96e609ba98d71e14ed898d54b1b95695b2036af8
Kirill Marenov <kmarenov@gmail.com>
2020-02-21 11:28:02 +0300

    99. useEffect() - упражнения

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
-const MyContext = React.createContext();
-
 const App = () => {
-    return (
-        <MyContext.Provider value="Hello World 123">
-            <Child />
-        </MyContext.Provider>
-    );
+    const [ value, setValue ] = useState(0);
+    const [ visible, setVisible ] = useState(true);
+    if (visible) {
+        return (
+            <div>
+                <button onClick={() => setValue(v => v + 1)}>+</button>
+                <button onClick={() => setVisible(false)}>Hide</button>
+                <HookCounter value={value} />
+                <Notification/>
+            </div>
+        );
+    }
+
+    else {
+        return (
+            <button onClick={() => setVisible(true)}>Show</button>
+        );
+    }
+
 };
 
+const HookCounter = ({ value }) => {
+    useEffect(() => console.log('mount'), []);
 
-const Child = () => {
-    const value = useContext(MyContext);
+    useEffect(() => console.log('update'));
+
+    useEffect(() => () => console.log('unmount'), []);
 
     return <p>{value}</p>;
+};
+
+const Notification = () => {
+    const [ visible, setVisible ] = useState(true);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => setVisible(false), 1500);
+        return () => clearTimeout(timerId);
+    }, []);
+
+    return visible ? <div><p>Hello!</p></div> : null;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
