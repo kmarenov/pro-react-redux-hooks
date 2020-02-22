0833a57a7e31897d29f9913958f1af090c7a2aec
Kirill Marenov <kmarenov@gmail.com>
2020-02-22 17:13:48 +0300

    100. Использование useEffect() для загрузки данных

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    const [ value, setValue ] = useState(0);
+    const [ value, setValue ] = useState(1);
     const [ visible, setVisible ] = useState(true);
     if (visible) {
         return (
@@ -10,7 +10,7 @@ const App = () => {
                 <button onClick={() => setValue(v => v + 1)}>+</button>
                 <button onClick={() => setVisible(false)}>Hide</button>
                 <HookCounter value={value} />
-                <Notification/>
+                <PlanetInfo id = {value} />
             </div>
         );
     }
@@ -21,6 +21,25 @@ const App = () => {
         );
     }
 
+};
+
+const PlanetInfo = ({ id }) => {
+    const [ name, setName ] = useState(null);
+
+    useEffect(() => {
+        let cancelled = false;
+
+        fetch(`https://swapi.co/api/planets/${id}`)
+            .then(res => res.json())
+            .then(data => !cancelled && setName(data.name))
+        ;
+
+        return () => cancelled = true;
+    }, [ id ]);
+
+    return (
+        <div>{id} - {name}</div>
+    );
 };
 
 const HookCounter = ({ value }) => {
