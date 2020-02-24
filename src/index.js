7c63e4cea0877c8086e1c7fa45e657784cef8bc4
Kirill Marenov <kmarenov@gmail.com>
2020-02-24 11:45:40 +0300

    102. useCallback() и useMemo()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
@@ -23,28 +23,66 @@ const App = () => {
 
 };
 
-const usePlanetInfo = (id) => {
-    const [ name, setName ] = useState(null);
+const getPlanet = (id) => {
+    return fetch(`https://swapi.co/api/planets/${id}`)
+        .then(res => res.json())
+        .then(data => data)
+    ;
+};
+
+const useRequest = (request) => {
+    const initialState = useMemo(() => {
+        return {
+            data: null,
+            loading: true,
+            error: null
+        }
+    }, []);
+
+    const [ dataState, setDataState ] = useState(initialState);
 
     useEffect(() => {
+        setDataState(initialState);
+
         let cancelled = false;
 
-        fetch(`https://swapi.co/api/planets/${id}`)
-            .then(res => res.json())
-            .then(data => !cancelled && setName(data.name))
+        request()
+            .then(data => !cancelled && setDataState({
+                data,
+                loading: false,
+                error: null
+            }))
+            .catch(error =>!cancelled && setDataState({
+                data: null,
+                loading: false,
+                error
+            }))
         ;
 
         return () => cancelled = true;
-    }, [ id ]);
+    }, [ request, initialState ]);
 
-    return name;
+    return dataState;
+};
+
+const usePlanetInfo = (id) => {
+    const request = useCallback(() => getPlanet(id), [ id ]);
+    return useRequest(request);
 };
 
 const PlanetInfo = ({ id }) => {
-    const name = usePlanetInfo(id);
+    const { data, loading, error } = usePlanetInfo(id);
+
+    if (error) {
+        return <div>Something is wrong</div>;
+    }
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     return (
-        <div>{id} - {name}</div>
+        <div>{id} - {data.name}</div>
     );
 };
 
@@ -58,7 +96,7 @@ const HookCounter = ({ value }) => {
     return <p>{value}</p>;
 };
 
-const Notification = () => {
+/*const Notification = () => {
     const [ visible, setVisible ] = useState(true);
 
     useEffect(() => {
@@ -67,6 +105,6 @@ const Notification = () => {
     }, []);
 
     return visible ? <div><p>Hello!</p></div> : null;
-};
+};*/
 
 ReactDOM.render(<App />, document.getElementById('root'));
